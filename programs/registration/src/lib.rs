94e786df3c694bdaedf1204c7ea4573cbc1c2e06
IggyDaddy <metaiggy@gmail.com>
2021-09-09 18:58:36 +0200

    Initialize registration program with a price

--- programs/registration/src/lib.rs
+++ programs/registration/src/lib.rs
@@ -5,10 +5,23 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod registration {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    pub fn initialize(ctx: Context<Initialize>, price: u8) -> ProgramResult {
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.price = price;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 1)]
+    pub my_account: Account<'info, MyAccount>,
+    pub user: AccountInfo<'info>,
+    pub system_program: AccountInfo<'info>,
+}
+
+#[account]
+pub struct MyAccount {
+    pub price: u8,
+}
--- tests/registration.js
+++ tests/registration.js
@@ -1,14 +1,33 @@
 const anchor = require('@project-serum/anchor');
+const assert = require("assert");
+const { SystemProgram } = anchor.web3;
 
 describe('registration', () => {
+  // Use a local provider.
+  const provider = anchor.Provider.local();
 
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  anchor.setProvider(provider);
 
   it('Is initialized!', async () => {
     // Add your test here.
     const program = anchor.workspace.Registration;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
+
+    const myAccount = anchor.web3.Keypair.generate();
+
+    await program.rpc.initialize(3, {
+      accounts: {
+        myAccount: myAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [myAccount],
+    });
+
+    // Fetch the newly created account from the cluster.
+    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+
+    // Check it's state was initialized.
+    assert.strictEqual(account.price, 3);
   });
 });
